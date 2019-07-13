175e12194e70519f8bc8404c30c834f0d5f23422
Kirill <kirillovk@yahoo.co.nz>
2019-07-13 15:49:35 +1200

    new branch for found elements activity

---
+++ app/src/main/java/org/tensorflow/lite/examples/detection/FoundElementActivity.kt
@@ -0,0 +1,12 @@
+package org.tensorflow.lite.examples.detection
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class FoundElementActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_found_element)
+    }
+}
